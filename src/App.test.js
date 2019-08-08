a8d5baa5e9ee2b9beab792203fb36574ed1125a6
nbstsh <nbstsh@gmail.com>
2019-08-08 18:47:30 +0900

    Testing to assert specific output

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,9 @@ import { shallow } from 'enzyme';
 it('renders without creasing', () => {
 	shallow(<App />);
 });
+
+it('renders welcome message', () => {
+	const wrapper = shallow(<App />);
+	const welcomeMessage = <h2>Welcome to React!!!</h2>;
+	expect(wrapper.contains(welcomeMessage)).toEqual(true);
+});
